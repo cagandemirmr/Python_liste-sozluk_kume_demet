4da8b0b7eaa82d4886e7e5a7a2c8a3b402b0f3f1
Cagan Demir <145290776+cagandemirmr@users.noreply.github.com>
2024-01-26 08:56:36 +0300

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,4 @@
+Gun=["pazartesi","salı","çarşamba","perşembe","cuma","cumartesi","pazar"]
+#enumate fonksiyonu for döngüsüyle kullanılır.
+for i, deger in enumerate(Gun):
+    print(str(i+1)+".gün "+deger)
